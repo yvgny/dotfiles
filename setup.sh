fe358c17dfdb4eff706b2c8e18b05c02cf35addf
Sacha Kozma <sacha.kozma@gmail.com>
2019-08-07 16:02:08 +0200

    Setup.sh handles git submodules and relative paths

--- setup.sh
+++ setup.sh
@@ -2,11 +2,17 @@
 EXCEPTION=('.gitignore' '.git' '.gitmodules' 'README.md' '.' '..' 'setup.sh')
 BACKUP_FOLDER='.dotfiles_backup'
 
+# Download Git submodules
+cd "$HOME/\.dotfiles"
+git submodule init && git submodule update --remote
+cd $HOME
+
 # Select of the config file
 REGEX=$(printf '%q|' "${EXCEPTION[@]}")
 REGEX=${REGEX::-1}
 REGEX="^($REGEX)$"
 FILE_LIST=$(ls -a .dotfiles/ | egrep -v $REGEX)
+
 # Backup conflicting config files
 mkdir $BACKUP_FOLDER
 mv $FILE_LIST $BACKUP_FOLDER 2>/dev/null
