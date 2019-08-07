32b084283a8e6d170d9b2c8cc79f451ed0f21eca
Sacha Kozma <sacha.kozma@gmail.com>
2019-08-07 15:46:57 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+EXCEPTION=('.gitignore' '.git' '.gitmodules' 'README.md' '.' '..' 'setup.sh')
+BACKUP_FOLDER='.dotfiles_backup'
+
+# Select of the config file
+REGEX=$(printf '%q|' "${EXCEPTION[@]}")
+REGEX=${REGEX::-1}
+REGEX="^($REGEX)$"
+FILE_LIST=$(ls -a .dotfiles/ | egrep -v $REGEX)
+# Backup conflicting config files
+mkdir $BACKUP_FOLDER
+mv $FILE_LIST $BACKUP_FOLDER 2>/dev/null
+
+# Remove backup folder if empty
+rmdir $BACKUP_FOLDER 2>/dev/null
+
+# Create symlink
+for filename in $FILE_LIST; do
+	ln -s ".dotfiles/$filename" .
+done
+
+echo 'Environnement has been set up'
